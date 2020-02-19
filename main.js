e1de783407b2041d71024d589522cc0b62d861fd
woczarder <wojtasessek1997@gmail.com>
2020-02-19 08:17:51 +0100

    started code

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("LOL");
+console.log("XD");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const image = document.getElementById('source');
+image.addEventListener('load', e => {
+    ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+});
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
 console.log("LOL")
+console.log("XD")
+
+const canvas = <HTMLCanvasElement> document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const image = <any> document.getElementById('source');
+
+image.addEventListener('load', e => {
+  ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+});
